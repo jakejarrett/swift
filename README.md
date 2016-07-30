f2443a8799349d410386ce777872bfec59e18093
Jake Jarrett <jakeryanjarrett@gmail.com>
2016-07-30 18:39:15 +1000

    helloworld added
    Messing with a helloworld program in swift, but instead of simply doing func helloWorld() etc, i wanted to mess around with classes too !

---
+++ README.md
@@ -0,0 +1,2 @@
+# Swift
+This is simply a few small files of me messing around with swift code. :smile_cat:
---
+++ helloworld.swift
@@ -0,0 +1,28 @@
+/**
+ * HelloWorld Class
+ *
+ * Put it into a class so i can get some better understanding of how classes work in Swift.
+ */
+class HelloWorld {
+
+    /**
+     * Returns a greeting
+     * @param  {String} username
+     * @return {String}             Returns a greeting with the users name.
+     */
+    func greetUser(username: String) -> String {
+        let greeting = "Hello " + username + "!";
+        return greeting;
+    }
+
+}
+
+/**
+ * Sugar syntax for accessing a class
+ */
+var helloWorld = HelloWorld();
+
+/**
+ * Print a greeting with the name "Jake"
+ */
+print(helloWorld.greetUser("Jake"));
